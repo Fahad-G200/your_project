3e729f1db8c9542b9db19205e00bfc68eb3915aa
Fahad-G200 <fah_08@icloud.com>
2025-12-05 13:21:43 +0100

    La inn funksjonen som åpner databaseforbindelsen, slik at registrering og innlogging kan hente og lagre brukere i MySQL-tabellen.

--- app.py
+++ app.py
@@ -4,3 +4,12 @@ from forms import RegisterForm, Loginform
 
 app = Flask(__name__)
 app.secret_key = "hemmelig-nok"
+
+
+def get_conn():
+    return mysql.connector.connect(
+        host="localhost",
+        user="DIN_DB_BRUKER",
+        password="DITT_PASSORD",
+        database="DIN_DATABASE" 
+    )
