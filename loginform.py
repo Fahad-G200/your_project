60e6fd58dbf5a996426be442eda15e6140164a92
Fahad-G200 <fah_08@icloud.com>
2025-12-12 14:29:08 +0100

    Ryddet opp i form-filer

--- loginform.py
+++
@@ -1,5 +0,0 @@
-class LoginForm(FlaskForm):
-    username = StringField("Brukernavn", validators=[InputRequired()])
-    password = PasswordField("Passord", validators=[InputRequired()])
-    submit = SubmitField("Logg inn")
-
