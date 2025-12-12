5ccfbcf66322db5aae93735bc16ecd27e3866127
Fahad-G200 <fah_08@icloud.com>
2025-12-12 14:30:32 +0100

    Ryddet opp i form-filer og fjernet duplikater

--- registerform.py
+++
@@ -1,7 +0,0 @@
-class RegisterForm(FlaskForm):
-    name = StringField("Navn", validators=[InputRequired()])
-    username = StringField("Brukernavn", validators=[InputRequired()])
-    password = PasswordField("Passord", validators=[InputRequired()])
-    submit = SubmitField("Registrer")
-
-
